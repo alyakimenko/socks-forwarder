11023cc4125472fc57f4142e16ed27a2fd4e5eac
alyakimenko <urlucidfall@gmail.com>
2020-05-13 13:49:21 +0300

    create basic setup

--- cmd/socks-forwarder/main.go
+++ cmd/socks-forwarder/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
+	"github.com/eycorsican/go-tun2socks/core"
+	"github.com/eycorsican/go-tun2socks/tun"
 	log "github.com/sirupsen/logrus"
 )
 
-const version = "0.01"
+const (
+	version = "0.01"
+	MTU     = 1500
+)
+
+var handlerCreater = make(map[string]func(), 0)
+
+func registerHandlerCreater(name string, creater func()) {
+	handlerCreater[name] = creater
+}
 
 func main() {
 	config := parseFlags()
@@ -30,4 +44,56 @@ func main() {
 	default:
 		panic("unsupport logging level")
 	}
+
+	// Open the tun device.
+	dnsServers := strings.Split(*config.TunDNS, ",")
+	tunDev, err := tun.OpenTunDevice(
+		*config.TunName, *config.TunAddr, *config.TunGw, *config.TunMask, dnsServers, *config.TunPersist,
+	)
+	if err != nil {
+		log.WithField(
+			"TUN Name", *config.TunName,
+		).Fatalf("failed to open tun device: %v", err)
+	}
+
+	// Setup TCP/IP stack.
+	lwipWriter := core.NewLWIPStack().(io.Writer)
+
+	// Register TCP and UDP handlers to handle accepted connections.
+	if creater, found := handlerCreater[*config.ProxyType]; found {
+		creater()
+	} else {
+		log.WithField(
+			"Proxy type", *config.ProxyType,
+		).Fatalf("unsupported proxy type")
+	}
+
+	if config.DNSFallback != nil && *config.DNSFallback {
+		// Override the UDP handler with a DNS-over-TCP (fallback) UDP handler.
+		if creater, found := handlerCreater["dnsfallback"]; found {
+			creater()
+		} else {
+			log.Fatalf("DNS fallback connection handler not found, build with `dnsfallback` tag")
+		}
+	}
+
+	// Register an output callback to write packets output from lwip stack to tun
+	// device, output function should be set before input any packets.
+	core.RegisterOutputFn(func(data []byte) (int, error) {
+		return tunDev.Write(data)
+	})
+
+	// Copy packets from tun device to lwip stack, it's the main loop.
+	go func() {
+		_, err := io.CopyBuffer(lwipWriter, tunDev, make([]byte, MTU))
+		if err != nil {
+			log.Fatalf("copying data failed: %v", err)
+		}
+	}()
+
+	log.Infof("Running tun2socks")
+
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGHUP)
+	<-osSignals
 }
