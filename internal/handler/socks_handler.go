c7f805ce87685d7848a05e9d4822ec30c46b54b5
alyakimenko <urlucidfall@gmail.com>
2020-05-14 11:17:42 +0300

    add logging on SOCKS handler register

--- internal/handler/socks_handler.go
+++ internal/handler/socks_handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/alyakimenko/socks-forwarder/config"
 	"github.com/eycorsican/go-tun2socks/core"
 	"github.com/eycorsican/go-tun2socks/proxy/socks"
+	log "github.com/sirupsen/logrus"
 )
 
 func RegisterSocksHandler(args *config.CmdArgs) error {
@@ -17,6 +18,12 @@ func RegisterSocksHandler(args *config.CmdArgs) error {
 	}
 	proxyHost := proxyAddr.IP.String()
 	proxyPort := uint16(proxyAddr.Port)
+
+	log.WithFields(log.Fields{
+		"Proxy Addr": *args.ProxyServer,
+		"Proxy Host": *args.ProxyHost,
+		"Proxy Port": *args.ProxyPort,
+	}).Info("Proxy details")
 
 	core.RegisterTCPConnHandler(socks.NewTCPHandler(proxyHost, proxyPort))
 	core.RegisterUDPConnHandler(socks.NewUDPHandler(proxyHost, proxyPort, *args.UDPTimeout))
